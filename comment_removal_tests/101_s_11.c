d1b71369c46a5bb77c894ab38df4d1e1601d891d
Hergoln <juliusz.malka@o2.pl>
2022-07-04 11:00:57 +0200

    added initialization handling

--- comment_removal_tests/101_s_11.c
+++ comment_removal_tests/101_s_11.c
@@ -17,7 +17,7 @@
 
 				}
 
-				for(int i= 0; i < size; i++) {
+				for(int i=0, j=1, g=2; i < size; i++) {
 
 				}
 
--- lexer/global.h
+++ lexer/global.h
@@ -6,9 +6,11 @@
 
 struct Symbol {
   std::string name;
+  std::string prefix;
   long no;
 };
 
 std::vector<Symbol> symtable;
 
 extern std::string handleName(std::string);
+std::string parse_initialization(const std::string yytext, char delimiter);
